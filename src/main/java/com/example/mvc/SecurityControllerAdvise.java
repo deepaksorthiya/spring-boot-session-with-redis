5261c99cf5a5f291de2d0899e701de3ee799f333
Deepak Katariya <deepak.sorthiya@gmail.com>
2025-11-10 23:07:58 +0530

    show logged-in username on ui

--- src/main/java/com/example/mvc/SecurityControllerAdvise.java
+++ src/main/java/com/example/mvc/SecurityControllerAdvise.java
@@ -4,14 +4,16 @@ import jakarta.servlet.http.HttpSession;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.security.Principal;
 
 @ControllerAdvice
 public class SecurityControllerAdvise {
 
     @ModelAttribute("currentUserName")
-    String currentUser(Principal principal) {
-        return (principal != null) ? principal.getName() : null;
+    String currentUser(Principal principal) throws UnknownHostException {
+        return (principal != null) ? principal.getName() + " on Host:: " + InetAddress.getLocalHost().getHostName() : null;
     }
 
     @ModelAttribute("httpSessionId")
